bb2f45c489e56521510bf0fcf8b5a000234beba9
Pablo Maribondo <pablomgvieira@gmail.com>
2024-09-20 21:46:39 -0300

    project setup

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import Fastify from "fastify";
+import crypto from "node:crypto";
+import { type User, UserService } from "./modules/user/user.service";
+import { AppError } from "./error/app.error";
+
+const app = Fastify();
+const userService = new UserService();
+
+app.get("/users", (request, reply) => {
+  try {
+    const users = userService.findAll();
+    return users;
+  } catch (err) {
+    reply.code(400).send({ error: err });
+  }
+});
+
+app.post("/users", (request, reply) => {
+  const authorization = request.headers.authorization;
+
+  if (!authorization) {
+    reply.code(401).send();
+  }
+
+  try {
+    const users = userService.create(request.body as User);
+    return users;
+  } catch (err) {
+    console.log("Error");
+    reply.code(400).send();
+  }
+});
+
+app.setErrorHandler((err, request, reply) => {
+  if (err instanceof AppError) {
+    reply.code(err.statusCode).send(err.message);
+  }
+  reply.code(500).send();
+});
+
+export { app };
